1c5e0cf65768ad1362805e4fd863939d2b01daed
ReskiKara <reskikara@gmail.com>
2025-06-12 12:22:26 +0800

    Fix build error and configure Tailwind properly

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,15 @@ module.exports = {
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
     "./resources/**/*.vue",
+    "./storage/framework/views/*.php",
+    "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Instrument Sans', 'ui-sans-serif', 'system-ui'],
+      },
+    },
   },
   plugins: [],
-} 
+}
