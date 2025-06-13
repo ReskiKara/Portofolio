59c5b847309f1b8695f9b6e7fe65594cf79df850
ReskiKara <reskikara@gmail.com>
2025-06-13 09:51:45 +0800

    Update Vercel function structure

--- api/index.php
+++ api/index.php
@@ -1,4 +1,16 @@
 <?php
 
-// Forward Vercel requests to normal index.php
-require __DIR__ . '/../public/index.php';
+// Require composer autoloader
+require __DIR__ . '/../vendor/autoload.php';
+
+// Create Laravel app
+$app = require __DIR__ . '/../bootstrap/app.php';
+
+// Run Laravel app
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
